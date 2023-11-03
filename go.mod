71dcbaf13d374b0ba4872d36365cb49224fb3ae7
agungphe <agungpasific@gmail.com>
2023-11-03 20:04:21 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/agungphe/belajarmoduleproject
+
+go 1.20
+
+require github.com/agungphe/belajarmodule v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	bm "github.com/agungphe/belajarmodule"
+)
+
+func main() {
+	fmt.Println(bm.SayHello())
+}
